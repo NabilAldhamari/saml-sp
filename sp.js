45002cb69b7b522dfbefef688ee07091a228caf5
Nabil Al-Dhamari <nabil.alli189@gmail.com>
2022-01-30 21:58:47 +0300

    Delete sp.js

--- sp.js
+++
@@ -1,9 +0,0 @@
-const SAML     = require("./saml-sp");
-const SSO_URL  = "http://localhost:8888/saml/consume";
-
-let sp = new SAML.ServiceProvider({
-    assertionEndpoint: SSO_URL
-});
-
-sp.saveRSAKeys(); // this will save the private key and certificate in the current directory
-sp.createMetaData(); // this will save the metadata.xml file in the current directory
